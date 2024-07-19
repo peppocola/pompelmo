f374bfe592d99b6ca102cf7e64e2cdf69caaaddc
peppocola <giuseppecolavito999@gmail.com>
2024-07-19 18:55:56 +0200

    update open graph settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     url: './',
     siteName: siteMetadata.title,
     images: [siteMetadata.socialBanner],
-    locale: 'en_US',
+    locale: 'it_IT',
     type: 'website',
   },
   alternates: {
