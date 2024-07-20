d4b461816566d0f931bac10b8e6f53f8747e614e
peppocola <giuseppecolavito999@gmail.com>
2024-07-20 14:33:40 +0200

    adapt newsletter form for mobile

--- app/newsletterform.tsx
+++ app/newsletterform.tsx
@@ -35,7 +35,7 @@ const NewsletterForm = ({
     const { error } = await res.json()
     if (error) {
       setError(true)
-      setMessage('La tua email non è valida o sei già iscrittooooooooooo.')
+      setMessage('La tua email non è valida o sei già iscritto.')
       return
     }
 
@@ -45,42 +45,36 @@ const NewsletterForm = ({
   }
 
   return (
-    <div>
+    <div className="mx-auto max-w-md">
       <div className="pb-1 text-lg font-semibold text-gray-800 dark:text-white">{title}</div>
       <form className="flex flex-col sm:flex-row" onSubmit={subscribe}>
-        <div>
-          <label htmlFor="email-input">
-            <span className="sr-only">Indirizzo email</span>
-            <input
-              autoComplete="email"
-              className="w-72 rounded-md px-4 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-hover-500 dark:bg-stone-900"
-              id="email-input"
-              name="email"
-              placeholder={subscribed ? 'Ti sei iscritto !  🎉' : 'Inserisci la tua email'}
-              ref={inputEl}
-              required
-              type="email"
-              disabled={subscribed}
-            />
-          </label>
-        </div>
-        <div className="mt-2 flex w-full rounded-md shadow-sm sm:ml-3 sm:mt-0">
-          <button
-            className={`w-full rounded-md bg-primary-600 px-4 py-2 font-medium text-white sm:py-0 ${
-              subscribed ? 'cursor-default' : 'hover:bg-hover-500 dark:hover:bg-hover-500'
-            } outline-none focus:ring-2 focus:ring-hover-500 focus:ring-offset-2 dark:ring-offset-black`}
-            type="submit"
+        <label htmlFor="email-input" className="flex-grow">
+          <span className="sr-only">Indirizzo email</span>
+          <input
+            autoComplete="email"
+            className="w-full rounded-md px-4 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-hover-500 dark:bg-stone-900"
+            id="email-input"
+            name="email"
+            placeholder={subscribed ? 'Ti sei iscritto !  🎉' : 'Inserisci la tua email'}
+            ref={inputEl}
+            required
+            type="email"
             disabled={subscribed}
-          >
-            {subscribed ? 'Grazie!' : 'Iscriviti'}
-          </button>
-        </div>
+          />
+        </label>
+        <button
+          className={`mt-2 w-full rounded-md bg-primary-600 px-4 py-2 font-medium text-white sm:ml-3 sm:mt-0 sm:w-auto ${
+            subscribed ? 'cursor-default' : 'hover:bg-hover-500 dark:hover:bg-hover-500'
+          } outline-none focus:ring-2 focus:ring-hover-500 focus:ring-offset-2 dark:ring-offset-black`}
+          type="submit"
+          disabled={subscribed}
+        >
+          {subscribed ? 'Grazie!' : 'Iscriviti'}
+        </button>
       </form>
-      {error && (
-        <div className="w-96 pt-2 text-sm text-red-500 dark:text-red-400 sm:w-96">{message}</div>
-      )}
+      {error && <div className="w-full pt-2 text-sm text-red-500 dark:text-red-400">{message}</div>}
       {subscribed && (
-        <div className="w-96 pt-2 text-sm text-green-500 dark:text-green-400 sm:w-96">
+        <div className="w-full pt-2 text-sm text-green-500 dark:text-green-400">
           La prima newsletter è in arrivo!🚀
           <br />
           Controlla la tua casella spam se non la trovi.
